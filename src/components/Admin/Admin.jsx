e0576dd8b7e1836469824ba37961defa494cf5a7
kbastamow <bastamowkat@gmail.com>
2023-05-11 17:40:04 +0200

    preview modal in progress

--- src/components/Admin/Admin.jsx
+++ src/components/Admin/Admin.jsx
@@ -46,9 +46,6 @@ useEffect(() => {
   setPreview(null)
 }}, [product])
 
-
-
-
 const handleCheckbox = (event) => {
   const value = event.target.value;
   const isChecked = event.target.checked;
@@ -78,57 +75,70 @@ const categoryMap = categories.map(category => {
 
   return (
     <>
-    <div>Admin</div>
+      <div>Admin</div>
 
-    <div><button type="button" className="btn btn-outline-danger">Create new product</button></div>
-        <div className="d-flex col-12 col-md-8 justify-content-center mx-auto">
-            
-            
-            <form id="post-form" encType="multipart/form-data" method="post" className="" onSubmit={create}>
-              <div className="form-outline mb-4">
-                <label className="form-label" htmlFor="productname">Name</label>
-                <input type="text"  className="form-control" name="name" id="productname" required  />
-              </div>
-              <div className="form-outline mb-4">
-                <label className="form-label" htmlFor="description">Description</label>
-                <textarea id="description" rows="4" className="form-control" name="description" required></textarea>
-              </div>
-              <div className="form-outline mb-4">
-                <label className="form-label" htmlFor="">Price</label>
-                <input type="number"  className="form-control" name="price" required id="price" />
-              </div>
+      <div><button type="button" className="btn btn-outline-danger">Create new product</button></div>
+      <div className="d-flex col-12 col-md-8 justify-content-center mx-auto">
 
+        <form id="post-form" encType="multipart/form-data" method="post" className="" onSubmit={create}>
 
-              <fieldset className="text-start">
-  <div className="text-center">Category</div>
-    {categoryMap}
+          <div className="form-outline mb-4">
+            <label className="form-label" htmlFor="productname">Name</label>
+            <input type="text" className="form-control" name="name" id="productname" required />
+          </div>
+          <div className="form-outline mb-4">
+            <label className="form-label" htmlFor="description">Description</label>
+            <textarea id="description" rows="4" className="form-control" name="description" required></textarea>
+          </div>
+          <div className="form-outline mb-4">
+            <label className="form-label" htmlFor="">Price</label>
+            <input type="number" className="form-control" name="price" required id="price" />
+          </div>
 
-  {/* <div>
-    <input type="checkbox" id="coding" name="interest" value="coding" />
-    <label htmlFor="coding">Coding</label>
+          <fieldset className="text-start">
+            <div className="text-center">Category</div>
+
+            {categoryMap}
+
+          </fieldset>
+          <div className="form-outline mb-4">
+            <label className="form-label" htmlFor="image">Upload an image</label>
+            <input type="file" id="image" name="image" accept="image/jpeg, image/png, image/jpg, image/gif" />
+          </div>
+          <button type="button" className="btn btn-primary" data-bs-toggle="modal" data-bs-target="#previewModal">Preview product</button>
+          
+          
+          <div className="modal fade" id="previewModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div className="modal-dialog">
+    <div className="modal-content">
+      <div className="modal-header">
+        <h5 className="modal-title" id="exampleModalLabel">Modal title</h5>
+        <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+      </div>
+      <div className="modal-body">
+        ...
+      </div>
+      <div className="modal-footer">
+        <button type="button" className="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+        <button type="button" className="btn btn-primary">Save changes</button>
+      </div>
+    </div>
   </div>
-  <div>
-    <input type="checkbox" id="music" name="interest" value="music" />
-    <label htmlFor="music">Music</label>
-  </div> */}
-</fieldset>
-
-
-
-
-              <div className="form-outline mb-4">
-                <label className="form-label" htmlFor="image">Upload an image</label>
-                <input type="file" id="image" name="image" accept="image/jpeg, image/png, image/jpg, image/gif"/>
-              </div>
-              <button type="submit" className="btn btn-primary btn-block col-12 mx-auto" required >Post</button>
-            </form>
-        </div>
-<div>
-  Hello
-  {preview}
 </div>
 
 
+          <button type="submit" className="btn btn-primary btn-block col-12 mx-auto" required >Post</button>
+
+
+
+
+
+
+        </form>
+      </div>
+      <div>
+        {preview}
+      </div>
     </>
 
   )
